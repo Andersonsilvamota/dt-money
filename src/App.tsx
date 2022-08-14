b0a2e1d55a5ee10ed931a82f1489e8aa0546ab98
Anderson Silva Mota <andersonsilv.mota@gmail.com>
2022-08-13 23:53:51 -0300

    Configuração de tema e fonte inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/themes/default"
 
 export function App() {
 
   return (
-    <h1>Olá mundo!</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Olá mundo!</h1>
+    </ThemeProvider>
   )
 }
